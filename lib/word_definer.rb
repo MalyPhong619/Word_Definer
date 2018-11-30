10036e44ab99e2e23adb4b780b84938242ccbd80
Student <example@example.com>
2018-11-30 10:28:53 -0800

    add class Word

---
+++ lib/word_definer.rb
@@ -0,0 +1,12 @@
+class Word
+  attr_accessor :term, :definition
+
+  def initialize(term, definition)
+      @term = word
+      @definition = definition
+  end
+
+  def word_term()
+    return @term
+  end
+end
