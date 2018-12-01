1cc3271296cf7dc15549b4eef14fb39e4393358e
Student <example@example.com>
2018-11-30 17:05:31 -0800

    show the definition once for is submitted

--- spec/word_definer_spec.rb
+++ spec/word_definer_spec.rb
@@ -2,8 +2,8 @@ require ('rspec')
 require ('./app')
 
 describe("Word") do
-  it("will initialize term and definition and return the term") do
-    newword = Word.new(:term=>"Apple", :definition=>"Round red fruit")
+  it("will initialize term and definition and both term and definition") do
+  newword = Word.new(:term=>"Apple", :definition=>"Round red fruit")
   expect(newword.word_term()).to(eq("Apple: Round red fruit"))
   end
 end
