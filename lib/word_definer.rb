1b5d72de981731e3a0f9d038686c1d7d86031be9
Student <example@example.com>
2018-12-08 16:51:36 -0800

    take in and initialize instances new Word - term and definition

--- lib/word_definer.rb
+++ lib/word_definer.rb
@@ -8,14 +8,6 @@ class Word
     @definition = attributes.fetch(:definition)
   end
 
-  def save()
-    @@list_of_terms.push(self)
-  end
-
-  def self.all()
-   @@list_of_terms.sort_by { |term| [:term]}
-  end
-
   def word_term()
     return @term + ": " + @definition
   end
--- spec/word_definer_spec.rb
+++ spec/word_definer_spec.rb
@@ -3,7 +3,7 @@ require ('./app')
 
 describe("Word") do
   it("will initialize term and definition and both term and definition") do
-  newword = Word.new(:term=>"Apple", :definition=>"Round red fruit")
-  expect(newword.word_term()).to(eq("Apple: Round red fruit"))
+  new_word = Word.new(:term=>"Apple", :definition=>"Round red fruit")
+  expect(new_word.word_term()).to(eq("Apple: Round red fruit"))
   end
 end
