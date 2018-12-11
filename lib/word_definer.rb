bc58cdff2ddcbfa46f0f13bfc056428ed6b3718e
Student <example@example.com>
2018-12-10 18:52:07 -0800

    add id instance to each object Word and main word_id start number

--- lib/word_definer.rb
+++ lib/word_definer.rb
@@ -1,20 +1,22 @@
 class Word
   @@list_of_words = []
+  @@word_id = 1
 
-    attr_accessor :term, :definition
+    attr_accessor :term, :definition, :id
 
   def initialize(attributes)
     @term = attributes.fetch(:term)
     @definitions = [attributes.fetch(:definition)]
+    @id = @@word_id
   end
 
   def save
     @@list_of_words.push(self)
   end
-  def self.all()
+
+  def self.all
     @@list_of_words
   end
-
 
   def add_definition(new_definition)
     @definitions.push(new_definition)
--- spec/word_definer_spec.rb
+++ spec/word_definer_spec.rb
@@ -1,19 +1,23 @@
 require ('rspec')
 require ('./app')
 
-describe("Word") do
-  it("will start class Word with empty array of objects") do
-    new_word = Word.new(:term=>"Apple", :definition=>"Round red fruit")
+describe('Word') do
+  it('will start class Word with empty array of objects') do
+    new_word = Word.new({:term=> 'Apple', :definition=> 'Round red fruit', :id=> nil})
     expect(Word.all).to(eq([]))
   end
 
-  it("will push a new Word into an array of Words") do
-    new_word = Word.new(:term=>"Apple", :definition=>"Round red fruit")
-    expect(new_word.save()).to(eq([new_word]))
+  describe('#save') do
+    it('will push a new Word into an array of Words') do
+      word = Word.new({:term=> 'Apple', :definition=> 'Round red fruit', :id=> nil})
+      expect(word.save()).to(eq([word]))
+    end
   end
 
-  it("will push a second definition into an array based on Term") do
-    apple1 = Word.new(:term => "Apple", :definition => "Round red fruit")
-    expect(apple1.add_definition("Grows on a Tree")).to(eq(["Round red fruit", "Grows on a Tree"]))
+  describe('#add_definition') do
+    it('will push a second definition into an array based on Term') do
+      word = Word.new({:term=> 'Apple', :definition=> 'Round red fruit', :id=> nil})
+      expect(word.add_definition('Grows on a Tree')).to(eq(['Round red fruit', 'Grows on a Tree']))
+    end
   end
 end
