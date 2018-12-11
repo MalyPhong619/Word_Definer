9ceb7c36a0f69b366834b2687cc3682982a6c5f3
Student <example@example.com>
2018-12-10 20:33:06 -0800

    add spec 1 to word_definer_integration_spec

--- spec/word_definer_integration_spec.rb
+++ spec/word_definer_integration_spec.rb
@@ -2,3 +2,13 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('Add a new term and definition', {:type => :feature}) do
+  it('creates a new word after user submits term and definition') do
+    visit('/')
+    fill_in('input_term', :with => 'Apple')
+    fill_in('input_definition', :with => 'Round red fruit.')
+    click_button('Submit!')
+    expect(page).to have_content('Apple')
+  end
+end
