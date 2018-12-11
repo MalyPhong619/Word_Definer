e9f75625d6dccfb05d9b5b5741da2085b89892da
Student <example@example.com>
2018-12-10 21:09:24 -0800

    corrects a href in input.erb

--- app.rb
+++ app.rb
@@ -5,16 +5,20 @@ require('./lib/word_definer')
 require('pry')
 
 get ('/') do
-  @list_of_words = Word.all()
+  @list_of_words = Word.all
   erb(:input)
 end
 
 post ('/') do
-  @list_of_words = Word.all()
+  @list_of_words = Word.all
   input_term = params["input_term"]
   input_definition = params["input_definition"]
-  word = Word.new({:term=>input_term, :definitions=>input_definition})
+  word = Word.new(:term=>input_term, :definitions=>input_definition)
   word.save()
-  @list_of_words = Word.all()
+  @list_of_words = Word.all
   erb(:input)
+end
+
+get ('output/.id') do
+  @list_of_words = Word.all
 end
