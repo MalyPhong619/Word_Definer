b23ab8b3e70596371e5f78565709479188f66be8
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2018-12-11 06:36:53 -0800

    use proper indentation and add spec to integration spec

--- spec/word_definer_integration_spec.rb
+++ spec/word_definer_integration_spec.rb
@@ -12,3 +12,12 @@ describe('Add a new term and definition', {:type => :feature}) do
     expect(page).to have_content('Apple')
   end
 end
+
+describe('Add a new definition', {:type => :feature}) do
+  it('creates a new definition after user submits form') do
+    visit('/output/1')
+    fill_in('input_definition', :with => 'Round red fruit.')
+    click_button('Submit!')
+    expect(page).to have_content('Round red fruit.')
+  end
+end
